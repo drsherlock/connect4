c1ee688592ab098266659cb866cbfe0846370d5c
Zain Haq <zain.haq@bookmyshow.com>
2020-09-27 20:08:31 +0530

    Update port

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
+const express = require('express');
 
-const routes = require('./routes')
+const routes = require('./routes');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
-app.use('/api', routes)
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
-})
+});
