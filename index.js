a79542ae4a950c30b6eba3e1adc7c4beb7dfc02d
Zain Haq <zain.haq@bookmyshow.com>
2020-09-29 02:10:43 +0530

    Add logger

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
+const morgan = require('morgan');
 
+var winston = require('./winston');
 const routes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(morgan('combined', { stream: winston.stream }));
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+  // render the error page
+  res.status(err.status || 500).send('Server Error');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 app.use('/api', routes);
+
+
+app.get('*', function(req, res){
+  winston.error(`404 - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+  res.status(404).send('Page Not Found');
+});
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
---
+++ winston.js
@@ -0,0 +1,39 @@
+const appRoot = require('app-root-path');
+const winston = require('winston');
+
+const options = {
+  file: {
+    level: 'info',
+    filename: `${appRoot}/logs/app.log`,
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: winston.format.combine(
+        winston.format.json()
+      )
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+    )
+  },
+};
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console)
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
