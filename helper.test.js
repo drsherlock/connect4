398370ec0cd0213aa464c133f680b63022e976be
drsherlock <zain.delhi27@hotmail.com>
2020-10-25 14:25:42 +0530

    Add tests for helper

--- helper.test.js
+++ helper.test.js
@@ -1,7 +1,10 @@
 
 describe('testing helpers', () => {
 
-	beforeEach(() => jest.resetModules());
+	beforeEach(() => {
+	    jest.unmock("./constants");
+	    jest.resetModules();
+	});
 
 	describe('reset board', () => {
 		test('with default constants', () => {
@@ -21,7 +24,7 @@ describe('testing helpers', () => {
 			expect(helper.resetBoard()).toEqual(game);
 		});
 
-		test('reset board', () => {
+		test('with mocked constants 1', () => {
 			const game = {
 				board: [
 					[0, 0, 0, 0],
@@ -38,7 +41,7 @@ describe('testing helpers', () => {
 			expect(helper.resetBoard()).toEqual(game);
 		});
 
-		test('reset board', () => {
+		test('with mocked constants 2', () => {
 			const game = {
 				board: [
 					[0, 0, 0, 0, 0],
@@ -53,6 +56,81 @@ describe('testing helpers', () => {
 
 			const helper = require('./helper');
 			expect(helper.resetBoard()).toEqual(game);
+		});
+	});
+
+	describe('make move', () => {
+		const testBoard = [
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0]
+				];
+
+		const helper = require('./helper');
+
+		test('player 1 move', () => {
+			const expectedBoard = [
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[1, 0, 0, 0, 0, 0, 0]
+				];
+
+			helper.makeMove(testBoard, 'YELLOW', 0);
+			expect(testBoard).toEqual(expectedBoard);
+		});
+
+		test('player 2 move', () => {
+			const expectedBoard = [
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[1, 2, 0, 0, 0, 0, 0]
+				];
+
+			helper.makeMove(testBoard, 'RED', 1);
+			expect(testBoard).toEqual(expectedBoard);
+		});
+	});
+
+	describe('check if win', () => {
+		test('player 1 does not win', () => {
+			const board = [
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0]
+				];
+
+			const helper = require('./helper');
+
+			expect(helper.checkIfWin(board, 'YELLOW')).toBe(false);
+
+		});
+
+		test('player 2 win', () => {
+			const board = [
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[0, 0, 0, 0, 0, 0, 0],
+					[2, 2, 2, 2, 0, 0, 0]
+				];
+
+			const helper = require('./helper');
+
+			expect(helper.checkIfWin(board, 'RED')).toBe(true);
+
 		});
 	});
 });
